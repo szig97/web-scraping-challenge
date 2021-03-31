e682de1fa07ff2a8ab52443ecf94e16f97f4efcf
Sam Ziegler <szig97@outlook.com>
2021-03-31 11:49:59 -0500

    defining scrape and dictionary

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -5,3 +5,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 #Site Naviagation
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
+
+# Defining scrape and dictionary
+def scrape():
+    final_data = {}
+    output = marsNews()
+    final_data["mars_news"] = output[0]
+    final_data["mars_paragraph"] = output[1]
+    final_data["mars_image"] = marsImage()
+    final_data["mars_facts"] = marsFacts()
+    final_data["mars_hemisphere"] = marsHem()
+
+    return final_data
