cc83d9eb5648c81d3728e096cafc9dcabf9320d4
Sam Ziegler <szig97@outlook.com>
2021-03-31 12:12:22 -0500

    defining marsFacts

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,6 +1,9 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd 
+import os
+import time
 
 #Site Naviagation
 executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -38,3 +41,14 @@ def marsImage():
     image = image_soup.find("img", class_="headerimage fade-in")["src"]
     featured_image_url = "https://spaceimages-mars.com/" + image
     return featured_image_url
+
+#Define marsFacts
+def marsFacts():
+    facts_url = "https://galaxyfacts-mars.com/"
+    browser.visit(facts_url)
+    mars_facts =pd.read_html(facts_url)
+    mars_facts_df = pd.DataFrame(mars_facts[0])
+    mars_facts_df = mars_facts_df.drop(columns = [2])
+    mars_facts_df = mars_facts_df.set_index(0)
+    mars_facts_html = mars_facts_df.to_html(header=False, index=False)
+    return mars_facts_html
